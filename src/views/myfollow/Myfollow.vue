922f47147ab0493bac08b06dd1a2b376c226cf29
BadRomance <1922148916@qq.com>
2020-10-26 10:19:37 +0800

    我的关注新加了一张图片、新加我的关注html和样式

--- src/views/myfollow/Myfollow.vue
+++ src/views/myfollow/Myfollow.vue
@@ -1,18 +1,116 @@
 <template>
-  <!-- 我的关注 -->
+  <!-- 热门推荐 -->
   <div class="myfollow">
-    11111
+    <div class="list_wrap">
+      <div class="list_nav">
+        <span>全部关注</span>
+        <!-- <span style="margin-left: 20px;" v-for="(v, i) in channel" :key="i">{{
+            
+            v.name
+          }}</span> -->
+        <span class="cur">国家</span>
+        <span>人物</span>
+        <span>媒体</span>
+        
+      </div>
+      <div class="list">
+        <ul>
+          <li>
+            <div class="ly">
+              <div class="tx">
+                <a href="">
+                <img src="../../assets/img/media.png" alt="">
+                </a>
+              </div>
+              <div class="sjrs">
+                  <span class="mt">新华社 </span>
+                <span class="time">时间: 10分钟前</span>
+                <span class="ll">浏览次数: 32人</span>
+              </div>
+               <div class="zan">
+                <img src="../../assets/img/zanpress.png" alt="" />
+                <span>214</span>
+                <img src="../../assets/img/zhuanfafasong.png" alt="" />
+              </div>
+
+            </div>
+            <div class="content">
+              <p class="title">
+                美国财长谈TikTok解决方案 不涉及出售和技术转让 
+              </p>
+              <div>
+                <img src="../../assets/img/gztp.png" alt="">
+                <img src="../../assets/img/gztp.png" alt="">
+              </div>
+              
+            </div>
+          </li>
+          <li>
+            <div class="ly">
+              <div class="tx">
+                <img src="../../assets/img/media.png" alt="">
+              </div>
+              <div class="sjrs">
+                  <span class="mt">媒体: TechWeb.com.cn </span>
+                <span class="time">时间: 10分钟前</span>
+                <span class="ll">浏览次数: 32人</span>
+              </div>
+               <div class="zan">
+                <img src="../../assets/img/zanpress.png" alt="" />
+                <span>214</span>
+                <img src="../../assets/img/zhuanfafasong.png" alt="" />
+              </div>
+
+            </div>
+            <div class="content">
+              <p class="title">
+                美国财长谈TikTok解决方案 不涉及出售和技术转让 
+              </p>
+              <div>
+                <img src="../../assets/img/gztp.png" alt="">
+                <img src="../../assets/img/gztp.png" alt="">
+              </div>
+              
+            </div>
+          </li>
+          <li>
+            <div class="ly">
+              <div class="tx">
+                <img src="../../assets/img/media.png" alt="">
+              </div>
+              <div class="sjrs">
+                  <span class="mt">媒体: TechWeb.com.cn </span>
+                <span class="time">时间: 10分钟前</span>
+                <span class="ll">浏览次数: 32人</span>
+              </div>
+               <div class="zan">
+                <img src="../../assets/img/zanpress.png" alt="" />
+                <span>214</span>
+                <img src="../../assets/img/zhuanfafasong.png" alt="" />
+              </div>
+
+            </div>
+            <div class="content">
+              <p class="title">
+                美国财长谈TikTok解决方案 不涉及出售和技术转让 
+              </p>
+              <div>
+                <img src="../../assets/img/gztp.png" alt="">
+                <img src="../../assets/img/gztp.png" alt="">
+              </div>
+              
+            </div>
+          </li>
+         
+          <div class="jzgd">更多精彩内容，加载中</div>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-@Component
-export default class MyFollow extends Vue {}
-</script>
+ 
 
 <style lang="scss" scoped>
-.myfollow {
-  
-}
+@import "./Myfollow.scss";
 </style>
