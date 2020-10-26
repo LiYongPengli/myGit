9172b6b5e507e2435dbf49471ec07442b5c1151d
BadRomance <1922148916@qq.com>
2020-10-26 16:04:50 +0800

    我的关注样式及html 还有首页banner图的效果

--- src/views/myfollow/Myfollow.vue
+++ src/views/myfollow/Myfollow.vue
@@ -11,22 +11,22 @@
         <span class="cur">国家</span>
         <span>人物</span>
         <span>媒体</span>
-        <div class="gjlb">
-          <p>选择相关国家可以进行信息筛选</p>
-          <span>全部</span>
+      </div>
+      <div class="gjlb">
+        <p class="sx">选择相关国家可以进行信息筛选</p>
+        <div class="erhang">
+          <span class="all">全部</span>
           <ul>
-            <li><a>中国</a></li>
+            <li><a>中国</a>X</li>
             <li><a>美国</a></li>
             <li><a>加拿大</a></li>
             <li><a>伯利兹</a></li>
             <li><a>墨西哥</a></li>
             <li><a>古巴</a></li>
             <li><a>瑞典</a></li>
-            <li><a>墨西哥</a></li>
+             <div class="gd">更多+</div>
           </ul>
-          <span class="gd">
-            更多+
-          </span>
+         
         </div>
       </div>
       <div class="list">
@@ -34,110 +34,94 @@
           <li>
             <div class="ly">
               <div class="tx">
-                <img src="../../assets/img/media.png" alt="">
+                <img src="../../assets/img/media.png" alt="" />
               </div>
               <div class="sjrs">
-                  <span class="mt">媒体: TechWeb.com.cn </span>
+                <span class="mt">媒体: TechWeb.com.cn </span>
                 <span class="time">时间: 10分钟前</span>
                 <span class="ll">浏览次数: 32人</span>
               </div>
-               <div class="zan">
+              <div class="zan">
                 <img src="../../assets/img/zanpress.png" alt="" />
                 <span>214</span>
                 <img src="../../assets/img/zhuanfafasong.png" alt="" />
               </div>
-
             </div>
             <div class="content">
-              <p class="title">
-                美国财长谈TikTok解决方案 不涉及出售和技术转让 
-              </p>
-             <div class="ziyuan">
-                <img src="../../assets/img/gztp.png" alt="">
-                <img src="../../assets/img/gztp.png" alt="">
+              <p class="title"><a href="">美国财长谈TikTok解决方案 不涉及出售和技术转让</a></p>
+              <div class="ziyuan">
+                <img src="../../assets/img/gztp.png" alt="" />
+                <img src="../../assets/img/gztp.png" alt="" />
               </div>
-              
             </div>
           </li>
           <li>
             <div class="ly">
               <div class="tx">
-                <img src="../../assets/img/media.png" alt="">
+                <img src="../../assets/img/media.png" alt="" />
               </div>
               <div class="sjrs">
-                  <span class="mt">媒体: TechWeb.com.cn </span>
+                <span class="mt">媒体: TechWeb.com.cn </span>
                 <span class="time">时间: 10分钟前</span>
                 <span class="ll">浏览次数: 32人</span>
               </div>
-               <div class="zan">
+              <div class="zan">
                 <img src="../../assets/img/zanpress.png" alt="" />
                 <span>214</span>
                 <img src="../../assets/img/zhuanfafasong.png" alt="" />
               </div>
-
             </div>
             <div class="content">
-              <p class="title">
-                美国财长谈TikTok解决方案 不涉及出售和技术转让 
-              </p>
-               <div class="ziyuan">
-                <img src="../../assets/img/gzxwdt.png" alt="">
-                 
+             <p class="title"><a href="">美国财长谈TikTok解决方案 不涉及出售和技术转让</a></p>
+              <div class="ziyuan">
+                <img src="../../assets/img/gzxwdt.png" alt="" />
               </div>
-              
             </div>
           </li>
-            <li>
+          <li>
             <div class="ly">
               <div class="tx">
-                <img src="../../assets/img/media.png" alt="">
+                <img src="../../assets/img/media.png" alt="" />
               </div>
               <div class="sjrs">
-                  <span class="mt">媒体: TechWeb.com.cn </span>
+                <span class="mt">媒体: TechWeb.com.cn </span>
                 <span class="time">时间: 10分钟前</span>
                 <span class="ll">浏览次数: 32人</span>
               </div>
-               <div class="zan">
+              <div class="zan">
                 <img src="../../assets/img/zanpress.png" alt="" />
                 <span>214</span>
                 <img src="../../assets/img/zhuanfafasong.png" alt="" />
               </div>
-
             </div>
             <div class="content">
-              <p class="title">
-                美国财长谈TikTok解决方案 不涉及出售和技术转让 
-              </p>
+              <p class="title"><a href="">美国财长谈TikTok解决方案 不涉及出售和技术转让</a></p>
             </div>
           </li>
-              <li>
+          <li>
             <div class="ly">
               <div class="tx">
                 <a href="">
-                <img src="../../assets/img/media.png" alt="">
+                  <img src="../../assets/img/media.png" alt="" />
                 </a>
               </div>
               <div class="sjrs">
-                  <span class="mt">新华社 </span>
+                <span class="mt">新华社 </span>
                 <span class="time">时间: 10分钟前</span>
                 <span class="ll">浏览次数: 32人</span>
               </div>
-               <div class="zan">
+              <div class="zan">
                 <img src="../../assets/img/zanpress.png" alt="" />
                 <span>214</span>
                 <img src="../../assets/img/zhuanfafasong.png" alt="" />
               </div>
-
             </div>
             <div class="content">
-              <p class="title">
-                美国财长谈TikTok解决方案 不涉及出售和技术转让 
-              </p>
+              <p class="title"><a href="">美国财长谈TikTok解决方案 不涉及出售和技术转让</a></p>
               <div class="ziyuan">
-                <img src="../../assets/img/gztp.png" alt="">
-                <img src="../../assets/img/gztp.png" alt="">
+                <img src="../../assets/img/gztp.png" alt="" />
+                <img src="../../assets/img/gztp.png" alt="" />
               </div>
-              
             </div>
           </li>
           <div class="jzgd">更多精彩内容，加载中</div>
