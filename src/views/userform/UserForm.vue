09cb585dfc4c6a9300702d518f926c8c25ec46f9
liyongpeng <liyongpeng@bjceis.com>
2020-10-30 14:58:45 +0800

    三级路由显示

--- src/views/userform/UserForm.vue
+++ src/views/userform/UserForm.vue
@@ -1,6 +1,11 @@
 <template>
 <!-- 个人中心 统计报表 -->
-  <div class="userform"></div>
+  <div class="userform">
+    <div class="top_nav">
+      <router-link tag="div" to="/user/form/fetch">数据采集统计</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
