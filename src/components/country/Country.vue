8a743f37f80730e86c7b5d51b4c1a6df77207fd9
BadRomance <1922148916@qq.com>
2020-11-17 18:04:53 +0800

    我的关注-国家页面

--- src/components/country/Country.vue
+++ src/components/country/Country.vue
@@ -1,17 +1,272 @@
 <template>
-<!-- 个人中心 我的关注 国家列表 -->
-  <div class="country"></div>
+  <!-- 个人中心 我的关注 国家列表 -->
+  <div class="country">
+    <!-- 已关注 -->
+    <div class="concerned">
+      <p class="concerned_p">已关注</p>
+      <ul>
+        <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!-- 未关注 -->
+    <div class="noattention">
+      <p class="noattention_p">未关注</p>
+      <p class="open">
+        展开
+        <img src="../../assets/img/open.png" alt="">
+      </p>
+       <ul>
+        <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+         <li>
+          <div>
+            <img src="../../assets/img/followchina.png" alt="">
+            <p>中国<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/img/followjnd.png" alt="">
+            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Component, { mixins } from 'vue-class-component';
-import CountryCom from './Country'
+import Component, { mixins } from "vue-class-component";
+import CountryCom from "./Country";
 @Component
-export default class Country extends mixins(CountryCom) {
-
-}
+export default class Country extends mixins(CountryCom) {}
 </script>
 
 <style lang="scss" scoped>
-@import './Country.scss';
+@import "./Country.scss";
 </style>
