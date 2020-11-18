4ad9a70a37f39682a0dc148830295f24fc6a2946
BadRomance <1922148916@qq.com>
2020-11-18 17:48:24 +0800

    用户管理相关页面样式调整以及开发操作成功的弹出框

--- src/components/userusersaccount/UserUsersAccount.vue
+++ src/components/userusersaccount/UserUsersAccount.vue
@@ -30,7 +30,10 @@
       <div class="left">
         <p>当前账户手机号</p>
         <p>{{ user.phone_number }}</p>
-        <el-button @click="changePhone=true" type="primary" style="margin-top: 20px; width: 150px"
+        <el-button
+          @click="changePhone = true"
+          type="primary"
+          style="margin-top: 20px; width: 150px"
           >更换手机号</el-button
         >
       </div>
@@ -41,41 +44,84 @@
           @click="resetPassword"
           type="primary"
           style="margin-top: 20px; width: 150px"
-          >重置密码</el-button>
+          >重置密码</el-button
+        >
       </div>
     </div>
     <!-- 重置密码 -->
     <el-dialog
       :visible.sync="resetPwd"
       title="重置密码"
+      class="czmm"
       width="800px"
-      top="25vh"
+      top="41vh"
     >
       <div class="restMode">
-        <p class="title">重置密码成功</p>
-        <p class="newPwd">新密码为:<span ref="newPwd">{{resPwd}}</span><span @click.stop="copyPwd">复制</span></p>
+        <p class="title">密码已重置成功</p>
+        <p class="newPwd">
+          新密码为:<span ref="newPwd">{{ resPwd }}</span
+          ><span @click.stop="copyPwd">复制</span>
+        </p>
         <p class="message">密码通过手机号短信发送,请提醒用户留意短信</p>
         <div class="resBtn">
-          <el-button @click="resetPwd=false" style="width:200px;margin-top:20px;" type="primary">确定</el-button>
+          <el-button
+            @click="resetPwd = false"
+            style="width: 190px; margin-top: 20px"
+            type="primary"
+            >确定</el-button
+          >
         </div>
       </div>
     </el-dialog>
     <!-- 更换手机号 -->
-    <el-dialog :visible.sync="changePhone"
+    <el-dialog
+      :visible.sync="changePhone"
       title="更换手机号"
-      width="700px"
-      top="25vh">
+      class="ghsjh"
+      width="800px"
+      top="40vh"
+    >
       <div class="changePhone">
         <div class="changePhone_top">
           <span>新手机号:</span>
-        <input type="number" v-model="phone" placeholder="请输入新的手机号码" />
+          <input
+            type="number"
+            v-model="phone"
+            placeholder="请输入新的手机号码"
+          />
         </div>
         <div class="changePhone_foot">
-          <el-button @click="changePhones" style="width:150px;" type="primary">确认</el-button>
-          <el-button @click="changePhone=false" style="width:150px;">取消</el-button>
+          <el-button @click="changePhones" style="width: 190px" type="primary"
+            >确认</el-button
+          >
+          <el-button @click="changePhone = false" style="width: 190px"
+            >取消</el-button
+          >
         </div>
       </div>
-      </el-dialog>
+    </el-dialog>
+    <!-- 操作成功 -->
+    <el-dialog
+      :visible.sync="changePhones"
+      title="温馨提示"
+      class="czmm"
+      width="800px"
+      top="41vh"
+    >
+      <div class="reminder">
+       <img  src="../../assets/img/czcg.png" alt="">
+       <p class="reminder_sucess" >操作成功</p>
+        <div class="resBtn">
+          <el-button
+            @click="resetPwd = false"
+            style="width: 190px; margin-top: 20px"
+            type="primary"
+            >确认</el-button
+          >
+        </div>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -100,10 +146,50 @@ export default class UserUsersAccount extends mixins(UserUsersAccountCom) {}
     }
   }
   .el-button--primary {
-   
-    background-color: #0074ff;
-    border-color: #409EFF;
+    // background-color: #0074ff;
+    // border-color: #409EFF;
     font-size: 16px;
-}
+  }
+  .ghsjh{
+    .el-dialog{
+      height: 330px;
+      .el-dialog__header{
+        padding-top: 30px;
+        height: 80px;
+        padding-left: 50px;
+        border-bottom: 1px solid #4d4d5d;
+        .el-dialog__title{
+          font-size: 20px;
+        }
+        .el-dialog__headerbtn{
+          margin-right: 30px;
+          color: white;
+          top: 30px;
+        }
+      }
+      .el-dialog__body{
+        padding: 60px 20px;
+      }
+    }
+  }
+  .czmm{
+    .el-dialog{
+      height: 380px;
+      .el-dialog__header{
+        padding-top: 30px;
+        height: 80px;
+        padding-left: 50px;
+        border-bottom: 1px solid #4d4d5d;
+        .el-dialog__title{
+          font-size: 20px;
+        }
+        .el-dialog__headerbtn{
+          margin-right: 30px;
+          color: white;
+          top: 30px;
+        }
+      }
+    }
+  }
 }
 </style>
