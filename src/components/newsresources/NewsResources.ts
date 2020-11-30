229c0f690ba4c4212471694400075fbb97113e8a
BadRomance <1922148916@qq.com>
2020-11-30 16:53:04 +0800

    资源使用率说明-新闻资源使用率静态页面开发

--- src/components/newsresources/NewsResources.ts
+++ src/components/newsresources/NewsResources.ts
@@ -9,278 +9,101 @@ export default class NewsResourcesCom extends Vue {
     public value1: string = "";
     public value2: string = "";
     public mounted(): void {
-
-        this.fetchDataInfo();
-        this.fetchDataInfo1();
-
-
-        // this.tableRowClassName();
-
     }
-    // <div class="list_li_accountnumber">账号</div>
-    //             <div class="list_li_name">昵称</div>
-    //             <div class="list_li_phone">手机号</div>
-    //             <div class="list_li_count">累计登录次数</div>
-    //             <div class="list_li_time">最后登录时间</div>
+
     public tableData = [{
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
-        // address: '上海市普陀区金沙江路 1518 弄'
+        ranking: '1',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '2',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '3',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '4',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
     },
     {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '5',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '6',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '7',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '8',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '9',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
     }, {
-        accountnumber: '张三',
-        name: 'Seekingalpha.com',
-        phone: '13011825913',
-        time: '2016-05-02 10:10:00',
-        count: '110次',
+        ranking: '10',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
+    }, {
+        ranking: '11',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
+    }, {
+        ranking: '12',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
+    }, {
+        ranking: '1',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
+    }, {
+        ranking: '13',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
+    }, {
+        ranking: '14',
+        newsname: '招银国际：新iPhone发布利好穿戴产品供应商',
+        forwardingtimes: '1000'
+
+
+    }, {
+        ranking: '15',
+        newsname: '招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支招银国际研报中称，苹果昨夜发布新产品，一如预期包括四款iPhone12，和智能扬声器HomePod Mini。四款iPhone12都支...',
+        forwardingtimes: '1000'
+
     }
     ]
 
-    //采集数据趋势图
-    private fetchDataInfo(): void {
-        let myChart2 = this.$refs.myChart2;
-        let myChart = echarts.init((myChart2 as HTMLDivElement));
-        let option: any = {
-            title: {
-                text: '采集数据趋势图',
-                subtext: '',
-                textStyle: {
-                    color: 'white'
-                }
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                data: ['浏览量', '同期对比'],
-                right: 100,
-                icon: 'rect',
-                textStyle: {
-                    color: "white"
-                }
-            },
-            toolbox: {
-                show: false,
-                feature: {
-                    magicType: { show: true, type: ['stack', 'tiled'] },
-                    saveAsImage: { show: true }
-                }
-            },
-            xAxis: {
-                type: 'category',
-                axisLine: {
-                    show: false,
-                    lineStyle: {
-                        color: 'red'
-                    }
-                },
-                boundaryGap: true,
-                axisTick: {
-                    show: false
+ 
 
-                },
-                data: ['00:00-02:59', '03:00-05:59', '06:00-08:59', '09:00-11:59', '12:00-14:59', '15:00-17:59', '18:00-20:59', '21:00-23:59'],
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#cdd6e5'
-                    }
-                }
-
-            },
-            yAxis: {
-                type: 'value',
-                axisLine: {
-                    show: false
-                },
-                splitLine: {
-                    show: true,
-                    lineStyle: {
-                        color: ['#494959'],
-                        width: 1,
-                        type: 'solid'
-                    }
-                }, //去除网格线
-                axisTick: {
-                    show: false
-
-                }, axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#cdd6e5'
-                    }
-                }
-            },
-            series: [{
-                name: '浏览量',
-                type: 'line',
-                symbol: 'circle',     //设定为实心点
-                symbolSize: 10,   //设定实心点的大小
-                smooth: true,
-                data: [90000, 10000, 30000, 70000, 25000, 83000, 100000, 83000],
-                color: "#19d1ff"
-            },
-            {
-                name: '同期对比',
-                type: 'line',
-                symbol: 'circle',     //设定为实心点
-                symbolSize: 10,   //设定实心点的大小
-                smooth: true,
-                data: [30000, 18200, 43400, 79100, 39000, 30000, 90000, 79100],
-                color: '#f4516c'
-            }]
-
-        }
-        myChart.setOption(option);
-    }
-
-    //活动用户数趋势图
-    private fetchDataInfo1(): void {
-        let myChart3 = this.$refs.myChart3;
-        let myChart4 = echarts.init((myChart3 as HTMLDivElement));
-        let option1: any = {
-            title: {
-                text: '活动用户数趋势图',
-                subtext: '',
-                textStyle: {
-                    color: 'white'
-                }
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                data: ['用户数', '同期对比'],
-                right: 100,
-                icon: 'rect',
-                textStyle: {
-                    color: "white"
-                }
-            },
-            toolbox: {
-                show: false,
-                feature: {
-                    magicType: { show: true, type: ['stack', 'tiled'] },
-                    saveAsImage: { show: true }
-                }
-            },
-            xAxis: {
-                type: 'category',
-                axisLine: {
-                    show: false,
-                    lineStyle: {
-                        color: 'red'
-                    }
-                },
-                boundaryGap: true,
-                axisTick: {
-                    show: false
-
-                },
-                data: ['00:00-02:59', '03:00-05:59', '06:00-08:59', '09:00-11:59', '12:00-14:59', '15:00-17:59', '18:00-20:59', '21:00-23:59'],
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#cdd6e5'
-                    }
-                }
-
-            },
-            yAxis: {
-                type: 'value',
-                axisLine: {
-                    show: false
-                },
-                splitLine: {
-                    show: true,
-                    lineStyle: {
-                        color: ['#494959'],
-                        width: 1,
-                        type: 'solid'
-                    }
-                }, //去除网格线
-                axisTick: {
-                    show: false
-
-                }, axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#cdd6e5'
-                    }
-                }
-            },
-            series: [{
-                name: '用户数',
-                type: 'line',
-                symbol: 'circle',     //设定为实心点
-                symbolSize: 10,   //设定实心点的大小
-                smooth: true,
-                data: [90000, 10000, 30000, 70000, 25000, 83000, 100000, 83000],
-                color: "#19d1ff"
-            },
-            {
-                name: '同期对比',
-                type: 'line',
-                symbol: 'circle',     //设定为实心点
-                symbolSize: 10,   //设定实心点的大小
-                smooth: true,
-                data: [30000, 18200, 43400, 79100, 39000, 30000, 90000, 30000],
-                color: '#f4516c'
-            }]
-
-        }
-        myChart4.setOption(option1);
-    }
 
 }
--- src/components/siteresources/SiteResources.vue
+++ src/components/siteresources/SiteResources.vue
@@ -1,83 +1,12 @@
 <template>
   <!-- 平台访问数据 -->
   <div class="siteresources">
-    <div class="content">
-      <div class="content_num">
-        <ul>
-          <li>
-            <div class="peoplenum">
-              <p class="num">983</p>
-              <p class="name">当前在线人数</p>
-            </div>
-            <div class="yesterday">
-              <p class="name">昨日同时:671</p>
-              <p class="num">56.00%</p>
-            </div>
-          </li>
-          <li>
-            <div class="peoplenum">
-              <p class="num">983</p>
-              <p class="name">当前在线人数</p>
-            </div>
-            <div class="yesterday">
-              <p class="name">昨日同时:671</p>
-              <p class="num">56.00%</p>
-            </div>
-          </li>
-          <li>
-            <div class="peoplenum">
-              <p class="num">983</p>
-              <p class="name">当前在线人数</p>
-            </div>
-            <div class="yesterday">
-              <p class="name">昨日同时:671</p>
-              <p class="num">56.00%</p>
-            </div>
-          </li>
-          <li>
-            <div class="peoplenum">
-              <p class="num">983</p>
-              <p class="name">当前在线人数</p>
-            </div>
-            <div class="yesterday">
-              <p class="name">昨日同时:671</p>
-              <p class="num">56.00%</p>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div class="content_tu">
-        <div class="cjsjs">
-          <div class="cjsjs_head">
-            <el-button>今日</el-button>
-            <el-button type="primary">最近7天</el-button>
-            <el-button class="zjsst" type="primary">最近30天</el-button>
-            <el-date-picker v-model="value1" type="date" placeholder="开始日期">
-            </el-date-picker>
-            <span class="zhi">-</span>
-            <el-date-picker v-model="value2" type="date" placeholder="截止日期">
-            </el-date-picker>
-            <span class="cjsjs_head_search">
-              <img src="../../assets/img/search.png" alt="" />
-            </span>
-          </div>
-        </div>
-        <!-- 采集数据数图 -->
-        <div class="cjsjqst">
-          <div class="small">
-            <div ref="myChart2" class="myChart2"></div>
-          </div>
-        </div>
-        <!-- 活动用户数趋势图 -->
-        <div class="hdyhqst">
-          <div class="small">
-            <div ref="myChart3" class="myChart3"></div>
-          </div>
-        </div>
-      </div>
+   <div class="content">
+     
       <div class="content_form">
         <div class="content_form_one">
-          <span class="namelist">用户登录名单</span>
+          <span class="namelist">站点浏览次数统计</span>
+          <span class="today">今日</span>
           <span class="Cumulative">累计</span>
           <el-date-picker v-model="value1" type="date" placeholder="开始日期">
           </el-date-picker>
@@ -87,19 +16,16 @@
           <div class="searchdate">
             <img src="../../assets/img/search.png" alt="" />
           </div>
-          <input type="text" placeholder="账号/昵称/手机号" />
-          <img class="searchinput" src="../../assets/img/search.png" alt="" />
           <span class="export">导出</span>
         </div>
         <div class="content_form_list">
-          <ul>
+           <ul>
             <my-scroll style="content_mt_onescroll">
               <li class="list_li">
-                <div class="list_li_accountnumber">账号</div>
-                <div class="list_li_name">昵称</div>
-                <div class="list_li_phone">手机号</div>
-                <div class="list_li_count">累计登录次数</div>
-                <div class="list_li_time">最后登录时间</div>
+                <div class="list_li_accountnumber">排名</div>
+                <div class="list_li_name">新闻名称</div>
+                <div class="list_li_phone">转发次数</div>
+                
               </li>
               <li
                 v-for="(v, k) in tableData"
@@ -107,25 +33,21 @@
                 :class="{ active: k % 2 != 0 }"
               >
                 <div class="list_li_accountnumber">
-                  {{ v.accountnumber }}
+                  {{ v.ranking }}
                 </div>
                 <div class="list_li_name">
-                  {{ v.name }}
+                  {{ v.newsname }}
                 </div>
-                <div class="list_li_phone">
-                  {{ v.phone }}
+              <div class="list_li_phone value">
+                  {{ v.forwardingtimes }}
                 </div>
-                 <div class="list_li_count">
-                  {{ v.count }}
-                </div>
-                 <div class="list_li_time">
-                  {{ v.time }}
-                </div>
+             
               </li>
             </my-scroll>
           </ul>
         </div>
       </div>
+      
     </div>
   </div>
 </template>
