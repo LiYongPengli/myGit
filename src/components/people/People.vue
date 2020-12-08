446ccca2ebaf391d31f5b145c180218462889855
BadRomance <1922148916@qq.com>
2020-12-08 13:34:55 +0800

    个人中心-我的关注-人物静态页面开发

--- src/components/people/People.vue
+++ src/components/people/People.vue
@@ -1,262 +1,120 @@
 <template>
-    <div class="people">
-         <!-- 已关注 -->
+  <div class="people">
+    <!-- 已关注 -->
     <div class="concerned">
-      <p class="concerned_p">人物已关注</p>
-      <ul>
-        <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/close.png" alt=""></p>
-          </div>
-        </li>
-      </ul>
+      <p class="concerned_p">已关注</p>
+
+      <div class="concernedlists">
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+        <div class="concernedlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/close.png" alt="" />
+        </div>
+      </div>
     </div>
     <!-- 未关注 -->
     <div class="noattention">
-      <p class="noattention_p">人物未关注</p>
-      <p class="open">
-        展开
-        <img src="../../assets/img/open.png" alt="">
+      <p class="noattention_p">未关注</p>
+      <p class="noattention_list">
+          还没有找到想关注的人物？
       </p>
-       <ul>
-        <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div >
-            <img src="../../assets/img/followchina.png" alt="">
-            <p >中国1<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-         <li>
-          <div>
-            <img src="../../assets/img/followchina.png" alt="">
-            <p>中国<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src="../../assets/img/followjnd.png" alt="">
-            <p>加拿大<img src="../../assets/img/add.png" alt=""></p>
-          </div>
-        </li>
-      </ul>
+     <div class="noattentionlists">
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+        <div class="noattentionlist">
+          <img src="../../assets/img/xdd.png" alt="" />
+          <span>习近平</span>
+          <p>中国共产党中央委员会总书记</p>
+          <img src="../../assets/img/add.png" alt="" />
+        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 <script lang="ts">
 import Component, { mixins } from "vue-class-component";
