35f4843d2f342e97060c130669733ccf9d992414
liyongpengli <1757600083@qq.com>
2020-12-21 13:48:24 +0800

    speed组件修改

--- src/components/SpeedText.vue
+++ src/components/SpeedText.vue
@@ -1,8 +1,13 @@
 <template>
-  <div @mouseleave="reset" @mouseenter="move" class="speedtext" :style="{'width':width+'px','height':height+'px'}">
-      <div class="dom" ref="dom">
-          <slot></slot>
-      </div>
+  <div
+    @mouseleave="reset"
+    @mouseenter="move"
+    class="speedtext"
+    :style="{ width: width, height: height }"
+  >
+    <div class="dom" ref="dom">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,60 +15,63 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class SpeedText extends Vue {
-    @Prop({}) width!:number;
-    @Prop({}) height!:number;
-    //子元素与父元素宽度差值
-    public offset:number = 0;
-    //滚动定时器
-    public timer:number|null = null;
+  @Prop({}) width!: string;
+  @Prop({}) height!: string;
+  @Prop({ default: 0.3, type: Number }) speed!: number;
+  //子元素与父元素宽度差值
+  public offset: number = 0;
+  //滚动定时器
+  public timer: number | null = null;
 
-    public mounted():void{
-        let dom = <HTMLElement>this.$refs.dom;
-        if(dom.offsetWidth>this.width){
-            this.offset = dom.offsetWidth - this.width;
-            //this.move(dom,offset);
-        }
+  public mounted(): void {
+    let dom = <HTMLElement>this.$refs.dom;
+    let width = ~this.width.indexOf("%")?this.width.slice(0,this.width.length-1):this.width.slice(0,this.width.length-2);
+    if (dom.offsetWidth > Number(width)) {
+      this.offset = dom.offsetWidth - Number(width);
     }
+  }
 
-    public move():void{
-        if(!this.offset) return;
-        let dom = <HTMLElement>this.$refs.dom;
-        if(this.timer) return;
-        let flag = false;
-        this.timer = setInterval(()=>{
-            if(dom.offsetLeft<=0&&Math.abs(dom.offsetLeft)>=this.offset){
-                flag = false;
-            }else if(dom.offsetLeft>=0){
-                flag = true;
-            }
-            if(!flag){
-                dom.style.left = (dom.offsetLeft+1)+'px';
-            }else{
-                dom.style.left = (dom.offsetLeft-1)+'px';
-            }
-        },100)
-    }
+  public move(): void {
+    if (!this.offset) return;
+    let dom = <HTMLElement>this.$refs.dom;
+    if (this.timer) return;
+    let flag = false;
+    let left = dom.offsetLeft;
+    this.timer = setInterval(() => {
+      if (dom.offsetLeft <= 0 && Math.abs(dom.offsetLeft) >= this.offset) {
+        flag = false;
+      } else if (dom.offsetLeft >= 0) {
+        flag = true;
+      }
+      if (!flag) {
+          left+=this.speed
+      } else {
+          left -=this.speed
+      }
+      dom.style.left = left + "px";
+    }, 10);
+  }
 
-    public reset():void{
-         let dom = <HTMLElement>this.$refs.dom;
-        if(this.timer){
-            clearInterval(this.timer);
-            this.timer = null;
-        }
-        dom.style.left = 0+'px';
+  public reset(): void {
+    let dom = <HTMLElement>this.$refs.dom;
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
     }
+    dom.style.left = 0 + "px";
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.speedtext{
-    overflow: hidden;
-    position: relative;
-    .dom{
-        width: max-content;
-        position: absolute;
-        left: 0;
-        top: 0;
-    }
+.speedtext {
+  overflow: hidden;
+  position: relative;
+  .dom {
+    width: max-content;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
 }
 </style>
