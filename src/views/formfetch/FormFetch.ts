2abd4e5597f7e5af858b10829d3b635de82a6d52
liyongpengli <1757600083@qq.com>
2020-12-23 17:52:13 +0800

    数据采集统计

--- src/views/formfetch/FormFetch.ts
+++ src/views/formfetch/FormFetch.ts
@@ -1,6 +1,6 @@
 import { Component, Vue } from 'vue-property-decorator'
-import echarts from 'echarts'
-import { mixin } from 'vue/types/umd';
+import echarts, { ECharts } from 'echarts'
+import { baseApi } from '@/axios/axios';
 @Component
 export default class FormFetchCom extends Vue {
 
@@ -30,169 +30,238 @@ export default class FormFetchCom extends Vue {
         }]
 
     }
+    public dates:Date[] = [];
     public value1: string = "";
     public value2: string = "";
+    public day_data: string[] = []
+    public month_data: string[] = []
+    public date_data: string[] = []
+    public result_data: any = "";
+    private chart: ECharts | null = null;
+    private charts_option = {
+        title: {
+            text: '数据采集趋势图',
+            subtext: '',
+            textStyle: {
+                color: 'white'
+            }
+        },
+        tooltip: {
+            trigger: 'axis',
+            formatter(value: any) {
+                if (value[1]) {
+                    return `<div>
+                        <div style="display:flex;align-items: center;padding:10px 0;">
+                            <div style="width:15px;height:15px;background:${value[0].color};border-radius: 50%;"></div>
+                            <div style="margin-left:5px;">${value[0].name.split(" ")[0]}</div>
+                            <div style="margin-left:50px;">${value[0].value == 0 ? value[0].value : value[0].value ? value[0].value : '--'}</div>
+                        </div>
+                        <div style="display:flex;align-items: center;padding:10px 0;">
+                            <div style="width:15px;height:15px;background:${value[1].color};border-radius: 50%;"></div>
+                            <div style="margin-left:5px;">${value[1].name.split(" ")[0]}</div>
+                            <div style="margin-left:50px;">${value[1].value}</div>
+                        </div>`
+                } else {
+                    return `<div>
+                            <div style="display:flex;align-items: center;padding:10px 0;">
+                                <div style="width:15px;height:15px;background:${value[0].color};border-radius: 50%;"></div>
+                                <div style="margin-left:5px;">${value[0].name.split(" ")[0]}</div>
+                                <div style="margin-left:50px;">${value[0].value == 0 ? value[0].value : value[0].value ? value[0].value : '--'}</div>
+                            </div>
+                            </div>`
 
+                }
 
+            }
+        },
+        legend: {
+            data: ['本期', '上期'],
+            right: 100,
+            icon: 'rect',
+            textStyle: {
+                color: "white"
+            }
+        },
+        toolbox: {
+            show: false,
+            feature: {
+                magicType: { show: true, type: ['stack', 'tiled'] },
+                saveAsImage: { show: true }
+            }
+        },
+        xAxis: {
+            type: 'category',
+            axisLine: {
+                show: false,
+                lineStyle: {
+                    color: 'red'
+                }
+            },
+            boundaryGap: true,
+            axisTick: {
+                show: false
 
-    public tableData = [{
-        number: '1',
-        date: '2016-05-02 10:10:00',
-        name: '王小虎1',
-        // address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-        number: '2',
-        date: '2016-05-04 10:10:00',
-        name: '王小虎2',
-        // address: '上海市普陀区金沙江路 1517 弄'
-    }, {
-        number: '3',
-        date: '2016-05-01 10:10:00',
-        name: '王小虎3',
-        // address: '上海市普陀区金沙江路 1519 弄'
-    }, {
-        number: '4',
-        date: '2016-05-03 10:10:00',
-        name: '王小虎4',
-        // address: '上海市普陀区金沙江路 1516 弄'
-    },
-    {
-        number: '5',
-        date: '2016-05-04 10:10:00',
-        name: '王小虎5',
-        // address: '上海市普陀区金沙江路 1517 弄'
-    }, {
-        number: '6',
-        date: '2016-05-01 10:10:00',
-        name: '王小虎6',
-        // address: '上海市普陀区金沙江路 1519 弄'
-    }, {
-        number: '7',
-        date: '2016-05-03 10:10:00',
-        name: '王小虎7',
-        // address: '上海市普陀区金沙江路 1516 弄'
-    }, {
-        number: '8',
-        date: '2016-05-04 10:10:00',
-        name: '王小虎8',
-        // address: '上海市普陀区金沙江路 1517 弄'
-    }, {
-        number: '9',
-        date: '2016-05-01 10:10:00',
-        name: '王小虎9',
-        // address: '上海市普陀区金沙江路 1519 弄'
-    }, {
-        number: '10',
-        date: '2016-05-03',
-        name: '王小虎',
-        // address: '上海市普陀区金沙江路 1516 弄'
+            },
+            data: [] as string[],
+            axisLabel: {
+                show: true,
+                textStyle: {
+                    color: '#cdd6e5'
+                }
+            }
+
+        },
+        yAxis: {
+            type: 'value',
+            axisLine: {
+                show: false
+            },
+            splitLine: {
+                show: true,
+                lineStyle: {
+                    color: ['#494959'],
+                    width: 1,
+                    type: 'solid'
+                }
+            }, //去除网格线
+            axisTick: {
+                show: false
+
+            }, axisLabel: {
+                show: true,
+                textStyle: {
+                    color: '#cdd6e5'
+                }
+            }
+        },
+        series: [{
+            name: '本期',
+            type: 'line',
+            symbol: 'circle',     //设定为实心点
+            symbolSize: 10,   //设定实心点的大小
+            smooth: true,
+            data: [] as any[],
+            color: "#19d1ff"
+        },
+        {
+            name: '上期',
+            type: 'line',
+            symbol: 'circle',     //设定为实心点
+            symbolSize: 10,   //设定实心点的大小
+            smooth: true,
+            data: [] as any[],
+            color: '#f4516c'
+        }]
+
     }
-    ]
+    public search_form = {
+        stat_type: 'today',
+        time_from: '',
+        time_to: '',
+    }
+
+    //今天，7天，30天
+    public setDay(type:string): void {
+        this.search_form.stat_type = type;
+        this.search_form.time_from = "";
+        this.search_form.time_to = "";
+        this.getData();
+    }
+
+    public setDate():void{
+        if(!this.dates){
+            this.setDay('today');
+            return;
+        }
+        this.search_form.stat_type = 'custom'
+        this.search_form.time_from = this.dates[0].toLocaleDateString();
+        this.search_form.time_to = this.dates[1].toLocaleDateString();
+        this.getData();
+    }
+
+
+    private getData(): void {
+        let data: any = {}
+        for (let i in this.search_form) {
+            if (this.search_form[i]) {
+                data[i] = this.search_form[i]
+            }
+        }
+        this.axios
+            .post(baseApi.api2 + '/v1/cmd/', {
+                cmd: 'data_gathering_stat',
+                paras: data,
+            }).then(res => {
+                console.log(res.data.data);
+                this.result_data = res.data.data;
+                this.setChartsData();
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
+    //设置echarts显示配置项
+    private setChartsData(): void {
+        this.charts_option.series[0].data = this.result_data.recent_detail;
+        this.charts_option.series[1].data = this.result_data.last_detail;
+        this.date_data = [];
+        switch (this.search_form.stat_type) {
+            case 'today':
+                for (let i = 0; i < 24; i++) {
+                    this.date_data.push((i + 1) + 'h');
+                }
+                break;
+            case '7':
+                for (let i = 0; i < 7; i++) {
+                    this.date_data.push('第' + (i + 1) + '天');
+                }
+                break;
+            case '30':
+                for (let i = 0; i < 30; i++) {
+                    this.date_data.push('第' + (i + 1) + '天');
+                }
+                break;
+            case 'custom':
+                for (let i = 0; i < this.result_data.last_detail.length; i++) {
+                    this.date_data.push('第' + (i + 1) + '天');
+                }
+                break;
+        }
+        this.charts_option.xAxis.data = this.date_data;
+
+        (<ECharts>this.chart).setOption(<any>this.charts_option);
+    }
 
 
     public mounted(): void {
-        this.fetchDataInfo();
-
-
-        // this.tableRowClassName();
+        let myChart2 = <HTMLDivElement>this.$refs.myChart2;
+        this.chart = echarts.init(myChart2);
+        this.getData();
 
     }
-    //采集数据趋势图
-    private fetchDataInfo(): void {
-        let myChart2 = this.$refs.myChart2;
-        let myChart = echarts.init((myChart2 as HTMLDivElement));
-        let option: any = {
-            title: {
-                text: '数据采集趋势图',
-                subtext: '',
-                textStyle: {
-                    color: 'white'
-                }
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                data: ['本周', '上周'],
-                right: 100,
-                icon: 'rect',
-                textStyle: {
-                    color: "white"
-                }
-            },
-            toolbox: {
-                show: false,
-                feature: {
-                    magicType: { show: true, type: ['stack', 'tiled'] },
-                    saveAsImage: { show: true }
-                }
-            },
-            xAxis: {
-                type: 'category',
-                axisLine: {
-                    show: false,
-                    lineStyle: {
-                        color: 'red'
-                    }
-                },
-                boundaryGap: true,
-                axisTick: {
-                    show: false
 
-                },
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#cdd6e5'
-                    }
-                }
-
-            },
-            yAxis: {
-                type: 'value',
-                axisLine: {
-                    show: false
-                },
-                splitLine: {
-                    show: true,
-                    lineStyle: {
-                        color: ['#494959'],
-                        width: 1,
-                        type: 'solid'
-                    }
-                }, //去除网格线
-                axisTick: {
-                    show: false
-
-                }, axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#cdd6e5'
-                    }
-                }
-            },
-            series: [{
-                name: '本周',
-                type: 'line',
-                symbol: 'circle',     //设定为实心点
-                symbolSize: 10,   //设定实心点的大小
-                smooth: true,
-                data: [90000, 10000, 30000, 70000, 25000, 83000, 100000],
-                color: "#19d1ff"
-            },
-            {
-                name: '上周',
-                type: 'line',
-                symbol: 'circle',     //设定为实心点
-                symbolSize: 10,   //设定实心点的大小
-                smooth: true,
-                data: [30000, 18200, 43400, 79100, 39000, 30000, 90000],
-                color: '#f4516c'
-            }]
-
+    //格式化数据格式
+    public init_number(number: number): string {
+        if (!number) {
+            return "";
         }
-        myChart.setOption(option);
+        let str = ""
+        let num = number + '';
+        if (num.length > 3) {
+            for (let i = 0; i < num.length; i++) {
+                if (i % 3 == 0) {
+                    str += num[i] + ','
+                } else {
+                    str += num[i]
+                }
+            }
+            if (str[str.length - 1] == ',') {
+                str = str.slice(0, str.length - 1);
+            }
+        } else {
+            str = num;
+        }
+        return str;
     }
 
 
